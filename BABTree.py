bbd4bb90d03e2773e8de83ddef4fdab42831d314
Vito Elias <vito.elias9@gmail.com>
2024-04-25 16:14:53 -0300

    Solution class

---
+++ BABTree.py
@@ -0,0 +1,10 @@
+class BABTree():
+    def __init__(self, model):
+        self.model = model
+        self.Zd = float('inf')
+        self.Zp = float('-inf')
+        self.solutions = []
+        self.best_solution = None
+
+    def branch():
+        pass
---
+++ Solution.py
@@ -0,0 +1,17 @@
+class Solution():
+    def __init__(self, model, variables):
+        self.model = model
+        self.solution = self.model.optimize()
+        self.solutionValue = model.objective_value
+        self.variablesValues = [i.x for i in variables]
+        self.defineSolutionType()
+    
+    def defineSolutionType(self):
+        for i in self.variablesValues:
+            if float.is_integer(i):
+                continue
+            else:
+                self.solutionType = 'continuous'
+                return
+            
+        self.solutionType = 'integer'
